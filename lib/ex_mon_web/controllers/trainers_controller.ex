4588c54f799b5bdd89f191107b149da090cc6c18
Guilherme Rigotti <ggrigotti@gmail.com>
2021-01-26 12:50:08 -0300

    feat: update trainer

--- lib/ex_mon_web/controllers/trainers_controller.ex
+++ lib/ex_mon_web/controllers/trainers_controller.ex
@@ -1,18 +1,30 @@
 defmodule ExMonWeb.TrainersController do
   use ExMonWeb, :controller
 
-  action_fallback ExMonWeb.FallbackController
+  action_fallback(ExMonWeb.FallbackController)
 
-  def create(conn,  params) do
+  def create(conn, params) do
     params
     |> ExMon.create_trainer()
-    |> handle_response(conn)
+    |> handle_response(conn, "create.json", :created)
   end
 
   def delete(conn, %{"id" => id}) do
     id
     |> ExMon.delete_trainer()
     |> handle_delete(conn)
+  end
+
+  def show(conn, %{"id" => id}) do
+    id
+    |> ExMon.fetch_trainer()
+    |> handle_response(conn, "show.json", :ok)
+  end
+
+  def update(conn, params) do
+    params
+    |> ExMon.update_trainer()
+    |> handle_response(conn, "update.json", :ok)
   end
 
   defp handle_delete({:ok, _trainer}, conn) do
@@ -23,11 +35,11 @@ defmodule ExMonWeb.TrainersController do
 
   defp handle_delete({:error, _reason} = error, _conn), do: error
 
-  defp handle_response({:ok, trainer}, conn) do
+  defp handle_response({:ok, trainer}, conn, view, status) do
     conn
-    |> put_status(:ok)
-    |> render("create.json", trainer: trainer)
+    |> put_status(status)
+    |> render(view, trainer: trainer)
   end
 
-  defp handle_response({:error, _changeset} = error, _conn), do: error
+  defp handle_response({:error, _changeset} = error, _conn, _view, _status), do: error
 end
--- lib/ex_mon_web/views/trainers_view.ex
+++ lib/ex_mon_web/views/trainers_view.ex
@@ -13,4 +13,24 @@ defmodule ExMonWeb.TrainersView do
       }
     }
   end
+
+  def render("update.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at, updated_at: updated_at}}) do
+    %{
+      message: "Trainer updated",
+      trainer: %{
+        id: id,
+        name: name,
+        inserted_at: inserted_at,
+        updated_at: updated_at
+      }
+    }
+  end
+
+  def render("show.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at}}) do
+    %{
+      id: id,
+      name: name,
+      inserted_at: inserted_at
+    }
+  end
 end
