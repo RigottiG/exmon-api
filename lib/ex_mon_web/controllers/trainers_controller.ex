2af98bba09e818f044894b76ecca5817d595dbcf
Guilherme Rigotti <ggrigotti@gmail.com>
2021-01-26 10:42:08 -0300

    feat: delete trainer

--- lib/ex_mon_web/controllers/trainers_controller.ex
+++ lib/ex_mon_web/controllers/trainers_controller.ex
@@ -9,6 +9,20 @@ defmodule ExMonWeb.TrainersController do
     |> handle_response(conn)
   end
 
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMon.delete_trainer()
+    |> handle_delete(conn)
+  end
+
+  defp handle_delete({:ok, _trainer}, conn) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+  end
+
+  defp handle_delete({:error, _reason} = error, _conn), do: error
+
   defp handle_response({:ok, trainer}, conn) do
     conn
     |> put_status(:ok)
